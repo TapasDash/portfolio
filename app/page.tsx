4798a17bc7297ffa0af6ef789d8d2ed3240c8393
Tapas <tapasdash017@gmail.com>
2024-04-13 02:58:59 +0530

    feat: setting Acceternity UI and adding grid-dot background

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Navbar from "./(home)/components/Navbar";
 const page = () => {
   return (
     <div className="min-h-screen bg-black text-neutral-200">
-      <div className="max-w-7xl mx-auto py-6 px-5">
-        {/* we wont use max-w-lg bcoz nav needs to be streched as much as possible */}
-        <Navbar />
-        <HeroSection />
+      <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.13] bg-grid-black/[0.13]">
+        <div className="max-w-7xl mx-auto py-6 px-5">
+          {/* we wont use max-w-lg bcoz nav needs to be streched as much as possible */}
+          <Navbar />
+          <HeroSection />
+        </div>
       </div>
     </div>
   );
